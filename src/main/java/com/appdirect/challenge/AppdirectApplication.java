79dd7b33d1a64317d903dc25444db63b3725efaa
Maaz Khan <maazkhansgsits@gmail.com>
2015-08-23 18:33:33 -0700

    Initial Bootstrap skeleton

---
+++ src/main/java/com/appdirect/challenge/AppdirectApplication.java
@@ -0,0 +1,12 @@
+package com.appdirect.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppdirectApplication.class, args);
+    }
+}
